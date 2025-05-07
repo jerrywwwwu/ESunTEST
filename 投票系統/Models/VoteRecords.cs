1080f0a8c27b7541a5ff24671221dbff8a905023
jerry_wu <jerry_wu@mail.ils.com.tw>
2025-05-07 11:00:35 +0800

    20250507

---
+++ 投票系統/Models/VoteRecords.cs
@@ -0,0 +1,11 @@
+﻿namespace VoteOnline.Models
+{
+    public class VoteRecords
+    {
+        public int VoteId { get; set; }
+
+        public string UserName { get; set; }
+
+        public int VoteItemId { get; set; }
+    }
+}
