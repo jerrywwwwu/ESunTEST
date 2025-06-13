03543f622965ec32ecf7b7b75f9e9a1d30cb484e
jerry_wu <jerry_wu@mail.ils.com.tw>
2025-06-13 17:10:42 +0800

    nlog.config/Common

---
+++ 投票系統/Common/BaseAdapter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+namespace VoteOnline.Common
+{
+    public class BaseAdapter
+    {
+        protected readonly string _connectionString;
+
+        public BaseAdapter(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("VoteOnlineConnectstring");
+        }
+    }
+}
